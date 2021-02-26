87fc8addf1269842578b092b348e7c132ec145f8
Sohan.Shanbhag <sohan.shanbhag2006@gmail.com>
2021-02-26 13:52:34 +0530

    Added

---
+++ dictate.js
@@ -0,0 +1,13 @@
+var logoImg, canvas;
+
+function preload(){
+    logoImg = loadImage("images/logo.png");
+}
+
+function setup(){
+    canvas = createCanvas(windowWidth-100, windowHeight-100)
+}
+
+function draw(){
+
+}
